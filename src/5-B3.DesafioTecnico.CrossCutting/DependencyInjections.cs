25d2eb696786c0553e019a58b1aa583c3a4cdeb4
Eduardo Gomes <edugomesti@outlook.com>
2023-02-24 18:28:57 -0300

    chore(data): added connection string

--- src/5-B3.DesafioTecnico.CrossCutting/DependencyInjections.cs
+++ src/5-B3.DesafioTecnico.CrossCutting/DependencyInjections.cs
@@ -1,15 +1,18 @@
 ﻿using B3.DesafioTecnico.Data.Repositories.Base.MySql;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace B3.DesafioTecnico.CrossCutting
 {
     public static class DependencyInjections
     {
-        public static void AddDependencyInjection(this IServiceCollection services)
+        public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration["ConnectionString"];
+
             services.AddDbContext<MySqlDbContext>(options =>
-                options.UseMySql("", ServerVersion.AutoDetect(""), 
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), 
                     m => m.MigrationsHistoryTable("Migration_History")));
         }
     }
